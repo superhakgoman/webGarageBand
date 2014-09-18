17c858367e7d31be38cec7da0208335b488335d4
이건희 <flecher1@gmail.com>
2014-09-18 15:07:06 +0900

    레이아웃 개선
    검은 건반 추가, 스크롤 On/Off 추가, 터치한 키 표시

--- javascript/script.js
+++ javascript/script.js
@@ -29,109 +29,113 @@ var SoundPool = {
 		}
 	}
 };
-
-// function SoundPool(){
-// 	var instance; 
-// 	SoundPool = function SoundPool(){
-// 		return instance;
-// 	};
-// 	SoundPool.prototype = this;
-
-// 	instance = new SoundPool();
-// 	instance.constructor = SoundPool;
-
-// 	var pool = {};
-// 	instance.add = function(id, soundSrc){
-// 		pool[id] = function(){
-// 			var sound = new Audio();
-// 			sound.src = soundSrc;
-// 			sound.autoplay = false;
-// 			sound.loop = false;
-// 			sound.preload = true;
-// 			return sound;
-// 		}();
-// 	};
-// 	instance.getSound = function(id){
-// 		return pool[id];
-// 	};
-// 	instance.isLoadCompleted = function(){
-// 		var loadCompleted = 0;
-// 		for (var i in pool){
-// 			// console.log(pool[i]);
-// 			if (pool[i].readyState == 4) {
-// 				loadCompleted++;
-// 			}
-// 		}
-// 		var poolSize = Object.keys(pool).length;
-// 		if (poolSize === loadCompleted) {
-// 			return true;
-// 		} else{
-// 			return false;
-// 		}
-// 	};
-// 	instance.getPool = function(){
-// 		return pool;
-// 	};
-
-// 	return instance;
-// }
-
 function initSoundPool(){
-	SoundPool.add("do", "sounds/piano_simple/2c.wav");
-	SoundPool.add("re", "sounds/piano_simple/2d.wav");
-	SoundPool.add("mi", "sounds/piano_simple/2e.wav");
-	SoundPool.add("fa", "sounds/piano_simple/2f.wav");
-	SoundPool.add("sol", "sounds/piano_simple/2g.wav");
-	SoundPool.add("la", "sounds/piano_simple/2a.wav");
-	SoundPool.add("ti", "sounds/piano_simple/2b.wav");
-	SoundPool.add("do2", "sounds/piano_simple/3c.wav");
+	var tempF = function(str){
+		SoundPool.add(str, "sound/"+str+".mp3");
+	};
+
+	tempF("3A");
+	tempF("3As");
+	tempF("3B");
+	tempF("3C");
+	tempF("3Cs");
+	tempF("3D");
+	tempF("3Ds");
+	tempF("3E");
+	tempF("3F");
+	tempF("3Fs");
+	tempF("3G");
+	tempF("3Gs");
+	tempF("4A");
+	tempF("4As");
+	tempF("4B");
+	tempF("4C");
+	tempF("4Cs");
+	tempF("4D");
+	tempF("4Ds");
+	tempF("4E");
+	tempF("4F");
+	tempF("4Fs");
+	tempF("4G");
+	tempF("4Gs");
+	tempF("5C");
+
+	// SoundPool.add("3C", "sound/3C.wav");
+	// SoundPool.add("3C#", "sound/3C#.wav");
+}
+
+var canScroll = false;
+
+function changeKeyColor(key, pressed){
+	if (pressed === true) {
+		key.style.backgroundColor = "gray";
+	}
+	else{
+		if (key.id.length === 2) {
+			key.style.backgroundColor = "white";
+		}
+		else if (key.id.length === 3){
+			key.style.backgroundColor = "black";
+		}
+	} 
 }
 
 window.addEventListener("load", function(){
 	initSoundPool();
-	var t = setInterval(function(){
+	t = setInterval(function(){
 		console.log("Load sound resources...");
 		if (SoundPool.isLoadCompleted()) {
 			console.log("Complete.");
 			clearInterval(t);
-			var _keys = document.querySelectorAll("div.key");
 			var oldKey;
-			for (var i = 0; i < _keys.length; i++) {
-				var keyId = _keys[i].id;
-				console.log(SoundPool.getSound(keyId));
+			var newKey;
 
-				_keys[i].addEventListener("touchstart", function(id){
-					// console.log(this);
-					console.log("touchStart : " + id);
-					SoundPool.getSound(id).play();
-					oldKey = document.elementFromPoint(event.targetTouches[0].pageX, event.targetTouches[0].pageY).id;
-					console.log(oldKey);
-				}.bind(this, keyId), false);
+			document.body.addEventListener("touchstart", function(){
+				oldKey = document.elementFromPoint(event.targetTouches[0].pageX, event.targetTouches[0].pageY);
+				if (oldKey.className === "key") {
+					SoundPool.getSound(oldKey.id).play();
+					changeKeyColor(oldKey, true);
+				}
+			}, false);
 
-				_keys[i].addEventListener("touchmove", function(){
+			document.body.addEventListener("touchmove", function(){
+				if (canScroll === false) {
 					event.preventDefault();
-					var newKey = document.elementFromPoint(event.targetTouches[0].pageX, event.targetTouches[0].pageY).id;
-					if (oldKey !== newKey) {
-						console.log("key changed.");
-						console.log("oldKey : " + oldKey + ", newKey : " + newKey);
-						SoundPool.getSound(oldKey).pause();
-						SoundPool.getSound(oldKey).currentTime = 0;
-						SoundPool.getSound(newKey).play();
-						oldKey = newKey;
-					}
-					console.log("oldKey : " + oldKey + ", newKey : " + newKey);
-					// console.log(document.elementFromPoint(event.targetTouches[0].pageX, event.targetTouches[0].pageY).id);
-					// SoundPool.getSound(event.targetTouches[0].target.id).play();
-				}, false);
+				}
+				newKey = document.elementFromPoint(event.targetTouches[0].pageX, event.targetTouches[0].pageY);
+				if (newKey.className === "key" && oldKey.id !== newKey.id) {
+					console.log("key changed.");
+					SoundPool.getSound(oldKey.id).pause();
+					SoundPool.getSound(oldKey.id).currentTime = 0;						
+					changeKeyColor(oldKey, false);
+					changeKeyColor(newKey, true);
+					SoundPool.getSound(newKey.id).play();
+					oldKey = newKey;
+				}
+			}, false);
 
-				_keys[i].addEventListener("touchend", function(id){
-					console.log("touchEnd : " + id);
-					SoundPool.getSound(id).pause();
-					SoundPool.getSound(id).currentTime = 0;
-				}.bind(this, keyId), false);
-			}
-			var loadMessage = document.querySelector("section p");
-			document.getElementById("keyboard").removeChild(loadMessage);
+			document.body.addEventListener("touchend", function(){
+				var key = document.elementFromPoint(event.changedTouches[0].pageX, event.changedTouches[0].pageY);
+				console.log(key.id);
+				if (key.className === "key") {
+					changeKeyColor(key, false);
+					SoundPool.getSound(key.id).pause();
+					SoundPool.getSound(key.id).currentTime = 0;
+				}
+			}, false);
+
+			var scrollLock = document.getElementById("scrollLock");
+			scrollLock.addEventListener("touchstart", function(){
+				canScroll = !canScroll;
+				if (canScroll === true) {
+					scrollLock.innerText = "스크롤 끄기";
+				} else if (canScroll === false){
+					scrollLock.innerText = "스크롤 켜기";
+				}
+			}, false);
+
+			var loadMessage = document.querySelector("body p");
+			document.body.removeChild(loadMessage);
 		}
 	}, 10);
 }, false);
